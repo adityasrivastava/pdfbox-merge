0c408e042194e660cad3e6f03234c0b14e2e9ee2
adityasrivastava <adityasrivastava010@gmail.com>
2020-03-21 21:31:08 +0530

    clean up

--- src/main/java/com/sample/pdf/App.java
+++ src/main/java/com/sample/pdf/App.java
@@ -15,24 +15,6 @@ public class App
 {
     public static void main( String[] args ) throws IOException {
 
-
-
-
     }
 
-    public static List<InputStream> files() throws FileNotFoundException {
-        List<InputStream> files = new ArrayList<>();
-
-        FileInputStream file1 = new FileInputStream("samplepdf/1.pdf");
-        FileInputStream file2 = new FileInputStream("samplepdf/2.pdf");
-        FileInputStream file3 = new FileInputStream("samplepdf/3.pdf");
-        FileInputStream file4 = new FileInputStream("samplepdf/4.pdf");
-
-        files.add(file1);
-        files.add(file2);
-        files.add(file3);
-        files.add(file4);
-
-        return files;
-    }
 }
